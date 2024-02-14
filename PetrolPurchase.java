eab720b5e166da3323ba65fecb3ed76854374ac8
Deva <jacobangel147@gmail.com>
2024-02-14 17:22:51 +0100

    changes

---
+++ PetrolPurchase.java
@@ -0,0 +1,81 @@
+public class PetrolPurchase {
+
+    public String stationLocation;
+    public String petroleumType;
+    public int quantityPurchased;
+    public double price;
+    public double percentageDiscount;
+
+    public PetrolPurchase( String stationLocation, String petroleumType, int quantityPurchased, double price, double percentageDiscount){
+
+        this.stationLocation = stationLocation;
+
+        this.petroleumType = petroleumType;
+
+        this.quantityPurchased = quantityPurchased;
+
+        this.price = price;
+
+        this.percentageDiscount =percentageDiscount;
+
+
+    }
+
+    public void setStationLocation(String stationLocation){
+
+        this.stationLocation=stationLocation;
+
+    }
+    public void setPetroleumType(String petroleumType){
+        this.petroleumType = petroleumType;
+    }
+
+    public void setQuantityPurchased(int quantityPurchased){
+        this.quantityPurchased = quantityPurchased;
+    }
+
+    public void setPrice(double price){
+
+       this.price = price;
+
+    }
+
+    public void setPercentageDiscount(double percentageDiscount){
+
+        this.percentageDiscount = percentageDiscount;
+    }
+
+
+    public String getStationLocation(){
+        return stationLocation;
+    }
+
+    public String getPetroleumType(){
+        return petroleumType;
+    }
+
+    public int getQuantityPurchased(){
+
+        return quantityPurchased;
+
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    public double getPercentageDiscount(){
+        return percentageDiscount;
+    }
+    public double getPurchaseAmount(double price, double percentageDiscount){
+
+       double netPurchaseAmount = price-(percentageDiscount/100 *price);
+
+       return netPurchaseAmount;
+
+    }
+
+
+
+
+
+}
